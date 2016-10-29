86d1d00201cb3590d7d1bc475c08334b6de2a666
jamesyu <zetaentranceserious@gmail.com>
2016-10-29 16:10:52 +0800

    Implement before filter logic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
+  before_action :header
+
   def index
     response.write("Hi from ApplicationController")
+  end
+
+  private
+  def header
+    response.write("<h1>This is Header</h1>")
   end
 end
